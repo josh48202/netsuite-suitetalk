b43ae01a4df6762775be565710f72e6dd1732418
Josh Becker <jbecker@vtgus.com>
2025-02-05 09:49:48 -0500

    Added InventoryItem Resource

--- src/Facades/NetSuiteSuiteTalk.php
+++ src/Facades/NetSuiteSuiteTalk.php
@@ -6,12 +6,14 @@ use Illuminate\Support\Facades\Facade;
 use Wjbecker\NetSuiteSuiteTalk\NetSuiteSuiteTalkClient;
 use Wjbecker\NetSuiteSuiteTalk\Resources\CustomerResource;
 use Wjbecker\NetSuiteSuiteTalk\Resources\ExpenseReportResource;
+use Wjbecker\NetSuiteSuiteTalk\Resources\InventoryItemResource;
 use Wjbecker\NetSuiteSuiteTalk\Resources\OpportunityResource;
 use Wjbecker\NetSuiteSuiteTalk\Resources\SalesOrderResource;
 
 /**
  * @method static CustomerResource customer()
  * @method static ExpenseReportResource expenseReport()
+ * @method static InventoryItemResource inventoryItem()
  * @method static OpportunityResource opportunity()
  * @method static SalesOrderResource salesOrder()
  * @method static query()
